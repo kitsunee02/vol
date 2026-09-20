63195c00f94a14d788b01ffafc714319f7fb75ed
kitsunee02 <kitsunee02@proton.me>
2026-09-20 09:19:45 +0530

    removed "fixing patches"

--- ayakaui.sh
+++ ayakaui.sh
@@ -34,16 +34,6 @@ export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 export IGNORE_PATCH_ERRORS=true
 echo "======= Export Done ======"
 
-#Fixing patchs
-set +e
-git -C frameworks/av am --abort 2>/dev/null
-git -C frameworks/base am --abort 2>/dev/null
-git -C hardware/interfaces am --abort 2>/dev/null
-git -C packages/modules/Bluetooth am --abort 2>/dev/null
-git -C build/soong am --abort 2>/dev/null
-git -C system/sepolicy am --abort 2>/dev/null
-set -e
-
 #Go fix
 SOONG_FILE="build/soong/ui/execution_metrics/execution_metrics.go"; git checkout -- "$SOONG_FILE" 2>/dev/null; [ -f "$SOONG_FILE" ] && (echo "🔧 Re-patching execution_metrics.go safely..."; grep -q '"sort"' "$SOONG_FILE" || sed -i '/^import (/a\    "sort"' "$SOONG_FILE"; sed -i '/"maps"/d; /"slices"/d' "$SOONG_FILE"; sed -i 's/slices\.Sorted(maps\.Keys(\([^)]*\)))/func() []string { keys := make([]string, 0, len(\1)); for k := range \1 { keys = append(keys, k) }; sort.Strings(keys); return keys }()/' "$SOONG_FILE"; echo "✅ Fixed and patched successfully!") || echo "❌ Soong execution_metrics.go not found!"
 
